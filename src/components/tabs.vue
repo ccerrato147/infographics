dc6c9eb9f1e6bbcbf7d813c73139993b0e20f92b
eduar <eduardo.escalona.leyva@gmail.com>
2020-03-22 00:06:49 -0600

    Add tabs default and description

---
+++ src/components/tabs.vue
@@ -0,0 +1,100 @@
+<template>
+        <div class="graphics">
+            <div class="container">
+                <div  v-if="graphic.tabs.length > 0">
+                    <div  class="graphics__tabs">
+                        <button v-for="tab in graphic.tabs" v-on:click="selectedTag = tab.graphic" class="graphics__tabs__selector"
+                          :style="tabsStyle">
+                            <span >{{tab.selector}}</span>
+                        </button>
+                    </div>
+                    <div class="graphics__info">
+                        <g-image  :src="selectedTag" alt="Graphic Image" class="graphics__img"/>
+                        <p class="graphics__info__description">asdas</p>
+                    </div>
+
+
+                </div>
+                <div v-else>
+                    <g-image :src="graphic.thumbnail" alt="Graphic Image" class="graphics__img"/>
+                </div>
+                <div v-html="graphic.content" />
+
+            </div>
+        </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'tabs',
+        data: function () {
+            return {
+                selectedTag: ""
+            }
+        },
+        props: {
+            graphic: {
+                type: Object,
+                default: () => [],
+                required: true
+            }
+        },
+        mounted() {
+            this.selectedTag = this.graphic.tabs[0].graphic;
+        },
+        computed: {
+            tabsStyle :function() {
+                return {
+                    width: (100 / this.graphic.tabs.length) + '%',
+                };
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    $content-gutter: 2rem;
+    .container {
+        padding: 8rem 0 4rem 0;
+
+    }
+    .graphics {
+        &__info {
+            img{
+                display: inline-block;
+                width: 50%;
+            }
+            &__description {
+                font-size: 24px;
+                display: inline-block;
+                width: 50%;
+                vertical-align: top;
+                margin: 0;
+                padding: 10px;
+            }
+        }
+        &__tabs {
+            font-size: 0;
+            display: inline-block;
+            width: 100%;
+            background-color: red;
+            margin-bottom: 2rem;
+
+            &__selector {
+                display: inline-block;
+
+                border: none;
+                outline: none;
+                cursor: pointer;
+            }
+            &__selector:hover {
+                background-color: #ddd;
+            }
+        }
+
+        &__img {
+            display: inline-block;
+        }
+    }
+</style>
--- src/templates/Graphic.vue
+++ src/templates/Graphic.vue
@@ -1,26 +1,12 @@
 <template>
     <Layout>
-        <div class="project">
-            <div class="container">
-                <div v-for="tab in $page.graphic.tabs">
-                    <button v-on:click="selectedTag = tab.selector">{{tab.selector}}</button>
-                    <g-image v-if="selectedTag == tab.selector" :src="tab.graphic" alt="Graphic Image" class="graphics__img"/>
-                </div>
-
-            </div>
-        </div>
+        <tabs :graphic="$page.graphic"></tabs>
     </Layout>
 </template>
 <script>
+    import Tabs from "../components/tabs";
     export default {
-        data: function () {
-            return {
-               selectedTag: ""
-            }
-        },
-        mounted: function () {
-
-        }
+        components: {Tabs}
     }
 </script>
 
@@ -28,19 +14,17 @@
     query Graphic ($path: String!) {
         graphic: graphic(path: $path) {
             title
+            description
+            thumbnail
+            content
             tabs {
                 selector
                 description
                 graphic
+
             }
         }
     }
 </page-query>
 
-<style lang="scss" scoped>
-    $content-gutter: 2rem;
-    .container {
-        padding: 8rem 0 4rem 0;
-    }
-</style>
 
